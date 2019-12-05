0f2fa5967547719f7cf5fe6976c433ceb98ea809
Maksim Filippov <filippov0705@gmail.com>
2019-12-05 18:07:20 +0300

    feat: add default header template

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,13 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { UserMenu } from './UserMenu';
-import { HeaderBar } from './HeaderBar';
 import { menuStateChange } from '../action/HeaderActions';
+import SearchAppBar from './SearchAppBar'
 
 class Header extends Component {
   render() {
       return (
         <React.Fragment>
-            <HeaderBar 
-                menuStateChange={this.props.menuStateChange} 
-                isUserMenuActive={this.props.header.isUserMenuActive} 
-                navBarActive={this.props.navBarActive} />
-            <UserMenu isUserMenuActive={this.props.header.isUserMenuActive} />
+            <SearchAppBar />
         </React.Fragment>
       );
   }
