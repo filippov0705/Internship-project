b2b001a73c4d2c8116e01c8cdbb85a66e20027f2
Maksim Filippov <filippov0705@gmail.com>
2019-12-06 12:42:22 +0300

    feat: add add-filter bar to the procedures page

--- src/containers/Procedures.js
+++ src/containers/Procedures.js
@@ -1,13 +1,83 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { menuStateChange, redirectFromPage } from '../action/HeaderActions';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import withStyles from '@material-ui/core/styles/withStyles';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 import './App.css'
+
+const styles = theme => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  },
+  rootDiv: {
+    height: '80px',
+    backgroundColor: '#252a3048',
+    borderRadius: '5px',
+},
+container: {
+  display: 'flex',
+  flexWrap: 'wrap',
+},
+textField: {
+  marginLeft: theme.spacing(1),
+  marginRight: theme.spacing(1),
+  width: 200,
+  backgroundColor: 'white',
+  borderRadius: '5px',
+},
+});
+
 
 class Procedures extends Component {
 
     render() {
-        //TODO: add content of the procedures page
-        return null
+      const { classes } = this.props;
+
+        return (
+          <React.Fragment>
+            <CssBaseline />
+            <Container maxWidth="lg" >
+              <Typography 
+                component="div" 
+                style={{ 
+                  marginTop: '64px',
+                  backgroundColor: '#cfe8fc', 
+                  height: '78vh', 
+                  width: '100%',
+                  borderTopRightRadius: '5px',
+                  borderTopLeftRadius: '5px',
+                  }} >
+                <Grid 
+                  container
+                  className={classes.rootDiv}
+                  justify="space-between"
+                  alignItems="center">
+                        <Grid item xs={6}>
+                          <TextField
+                            id="outlined-search"
+                            label="Filter"
+                            type="search"
+                            className={classes.textField}
+                            margin="normal"
+                            variant="outlined"/>
+                        </Grid>
+                        <Grid item>
+                        <Button variant="contained" color="primary">
+                          ADD
+                        </Button>
+                    </Grid>
+                </Grid>
+              </Typography>
+            </Container>
+          </React.Fragment>
+        )
     }
 }
 
@@ -24,4 +94,4 @@ const mapStateToProps = store => {
     }
   }
   
-  export default connect(mapStateToProps, mapDispatchToProps)(Procedures)
+  export default withStyles(styles)(connect(mapStateToProps, mapDispatchToProps)(Procedures));
