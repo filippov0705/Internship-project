e2457b59030b5cc78e12d509f138383e545f446c
Maksim Filippov <filippov0705@gmail.com>
2019-12-09 09:28:25 +0300

    feat: add content to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,10 +3,22 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
+import FooterLogo from '../styles/img/footer-logo.svg';
+import withStyles from '@material-ui/core/styles/withStyles';
 
-export default class Footer extends React.Component {
-    render() {
-        return (
+const styles = theme => ({
+  columnGrid: {
+    display: 'flex', 
+    flexDirection: 'column',
+    fontSize: '14px',
+    color: 'white'
+  }
+})
+
+const Footer = props => {
+  const { classes } = props;
+
+  return (
             <React.Fragment>
             <CssBaseline />
             <Container 
@@ -23,16 +35,28 @@ export default class Footer extends React.Component {
                   alignItems: 'center'
                   }} >
                     <Grid container>
-                      <Grid item xs={3}  style={{backgroundColor: 'green'}}>
-                     
+                      <Grid item xs={4}   className={classes.columnGrid}>
+                        <img src={FooterLogo} alt="logo" style={{width: '150px'}} />
+                        <span>&#169; 2019 :iTechArt All Rights Reserved.</span>
                       </Grid>
-                      <Grid item xs={3}  style={{backgroundColor: 'green'}}>asdasd</Grid>
-                      <Grid item xs={3}  style={{backgroundColor: 'green'}}>asdasd</Grid>
-                      <Grid item xs={3}  style={{backgroundColor: 'green'}}>asdasd</Grid>
+                      <Grid item xs={4} className={classes.columnGrid}>
+                        <span>Телефоны:</span>
+                        <span>+375 33 38 76 163</span>
+                        <span>+375 29 15 71 703</span>
+                      </Grid>
+                      <Grid item xs={4} className={classes.columnGrid}>
+                        <span>E-mail:</span>
+                        <a href="mailto:careers@itechart-group.com">careers@itechart-group.com</a>
+                        <span>Старт карьеры:</span>
+                        <a href="mailto:st.lab@itechart-group.com">st.lab@itechart-group.com</a>
+                        <span>Сотрудничество:</span>
+                        <a href="mailto:pr@itechart-group.com">pr@itechart-group.com</a>
+                      </Grid>
                     </Grid>
                 </Typography>
             </Container>
           </React.Fragment>
-        );
-    }
-}
+        )
+                            }
+
+export default withStyles(styles)(Footer);
