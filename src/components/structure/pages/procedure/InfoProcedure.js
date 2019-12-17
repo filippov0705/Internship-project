c8ca5273e5496ff1fa82ee29379ad1047b41423e
Maksim Filippov <filippov0705@gmail.com>
2019-12-17 09:07:13 +0300

    feat/add heading to InfoProcedure

--- src/components/structure/pages/procedure/InfoProcedure.js
+++ src/components/structure/pages/procedure/InfoProcedure.js
@@ -1,28 +1,29 @@
-import React, { Component } from 'react';
-import Task from '../task/Tasks';
-import { connect } from 'react-redux';
-import ProcedurePage from './ProcedurePage';
+import React, { Component } from "react";
+import Task from "../task/Tasks";
+import { connect } from "react-redux";
+import ProcedurePage from "./ProcedurePage";
+import Heading from "../../../common/Heading";
 
 class InfoProcedure extends Component {
-
   render() {
-    const proceduresList = this.props.procedures.proceduresList;
-    const id = this.props.match.params.id;
-    const targetProcedure = proceduresList.find(item => item.id === id);
+    const targetProcedure = this.props.procedures.proceduresList.find(
+      item => item.id === this.props.match.params.id
+    );
     const data = targetProcedure ? targetProcedure.tasks : [];
 
     return (
       <ProcedurePage>
-        <Task data={data} content={'data'} />
+        <Heading size={"big"} heading={"Procedure taksk"} />
+        <Task data={data} content={"data"} />
       </ProcedurePage>
-    )
+    );
   }
 }
 
 const mapStateToProps = store => {
   return {
-      procedures: store.procedures
-  }
-}
+    procedures: store.procedures
+  };
+};
 
 export default connect(mapStateToProps)(InfoProcedure);
